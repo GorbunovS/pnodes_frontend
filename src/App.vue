1178a014c4af1a04c549de6a2ba76e18bb5163f5
GorbunovS <obes1stas1@gmail.com>
2025-12-23 21:12:52 +0300

    designUpdate

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <script setup>
 import NodesView from './components/NodesView.vue';
 
+import { onMounted } from 'vue'
+
+onMounted(() => {
+    // Блокируем светлую тему
+    document.documentElement.classList.add('dark-mode')
+    document.documentElement.style.colorScheme = 'dark'
+})
+
 </script>
 
 <template>
